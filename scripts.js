0bb088998245bfd8c01338cdec932769937fa7d8
Robx33 <robsonc25@outlook.com>
2023-03-13 16:07:35 -0300

    commit

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ const relogio = setInterval( function time(){
     let dateToday = new Date()
     let hr = dateToday.getHours()
     let min = dateToday.getMinutes()
-    let seg = dateToday.getSeconds()
+    let seg = dateToday.getSeconds()    
 
     if(hr < 10) hr = '0' + hr
     if(min < 10) min = '0' + min
